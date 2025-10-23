7373e8e6cfb8e61420400b3e8c4250511ae8d7a7
Vladimir Eremia <vladimireremia377@gmail.com>
2025-10-23 12:31:06 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lingua-puzzle-mobile',
+  webDir: 'www'
+};
+
+export default config;
